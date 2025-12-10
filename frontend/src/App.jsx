439e7680091d7dcdb0c941c5e79a7e25661eb56c
tabishcode <tabishakhtar007860@gmail.com>
2025-12-10 07:52:23 +0500

    core: set up frontend folder structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './Layout';
-import { socket } from './socket';
 
-function App() {
-  const [user, setUser] = useState(() => JSON.parse(localStorage.getItem('user')));
 
-  useEffect(() => {
-    if (user) {
-      // connect socket and notify online
-      socket.auth = { token: localStorage.getItem('token') };
-      socket.connect();
-      socket.emit('user:online', user.id);
-    } else {
-      socket.disconnect();
-    }
-    return () => socket.disconnect();
-  }, [user]);
+export default function App() {
+  // Auto getMe on page refresh
+ 
 
   return (
-    <>
-      <BrowserRouter>
-        <Layout user={user} setUser={setUser} />
-      </BrowserRouter>
-    </>
+   <>
+     Hello World
+   </>
   );
 }
-export default App;
